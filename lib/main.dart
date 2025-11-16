d5e10a3833fd6d1608a0b0ccfdcc1556849c2bf6
Niponpal <niponpal52@gmail.com>
2025-11-16 22:13:50 +0600

    Fix 🚀🚀🚑 Product Details

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:dadagarments/discountscreen.dart';
 import 'package:dadagarments/editprofilescreen.dart';
 import 'package:dadagarments/homescreen.dart';
 import 'package:dadagarments/loginScreen.dart';
+import 'package:dadagarments/productdetails.dart';
 import 'package:dadagarments/profilescreen.dart';
 import 'package:dadagarments/registerscreen.dart';
 import 'package:dadagarments/splashscreen.dart';
@@ -13,7 +14,7 @@ void main (){
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: EditprofilescreenView(),
+      home: ProductdetailsView(),
     ),
   );
 }
